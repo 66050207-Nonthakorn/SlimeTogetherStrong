2e72ce96d6af3a143c8a17cfbcf8fdc854e43da3
Nonthakorn Srichai <66050207@kmitl.ac.th>
2026-01-28 09:47:01 +0700

    Remove unused button initialization code from GameScene

--- Game/GameScene.cs
+++ Game/GameScene.cs
@@ -1,7 +1,4 @@
-using System;
 using SlimeTogetherStrong.Engine;
-using SlimeTogetherStrong.Engine.Managers;
-using SlimeTogetherStrong.Engine.UI;
 
 namespace SlimeTogetherStrong.Game;
 
@@ -9,19 +6,6 @@ class GameScene : Scene
 {
     public override void Load()
     {
-        GameObject button = new()
-        {
-            Position = new(300, 200)
-        };
 
-        button.AddComponent<Button>();
-        button.GetComponent<Button>().Text = "Start Game";
-        button.GetComponent<Button>().Font = ResourceManager.Instance.GetFont("DefaultFont");
-        button.GetComponent<Button>().OnClick = () =>
-        {
-            Console.WriteLine("Start Game button clicked!");
-        };
-
-        AddGameObject(button);
     }
 }
